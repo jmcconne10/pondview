775bd8a98eacaa5077f48d58016ed55e5dbfac20
Joe McConnell <joe.mcconnell10@gmail.com>
2021-04-17 15:14:37 -0500

    fourth

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the third update!
+          This is the fourth update!
         </p>
         <a
           className="App-link"
